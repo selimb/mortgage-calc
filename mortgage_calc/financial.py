36ed31d7b1d9b38adfa5f332a24d9ab1206a39c3
Selim Belhaouane <selim.belhaouane@gmail.com>
2023-08-26 16:20:42 -0400

    looks good

--- mortgage_calc/financial.py
+++ mortgage_calc/financial.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+import dataclasses
+import numpy_financial as nf
 from typing import Literal
+from typing_extensions import assert_never
 
 
 def PMT(rate: float, payments_count: int, principal: float) -> float:
@@ -17,20 +21,96 @@ def PMT(rate: float, payments_count: int, principal: float) -> float:
     return (r * P) / (1 - (1 + r) ** (-N))
 
 
-def rate_calc(rate_annual: float, compound: Literal["semi-annual"] = "semi-annual") -> float:
+def IPMT(rate: float, payment_number: int, payments_count: int, principal: float) -> float:
+    """Like IPMT from excel."""
+    return float(nf.ipmt(rate, payment_number, payments_count, principal)) * -1
+
+
+def rate_calc(
+    rate_annual: float, compound: Literal["semi-annual", "month"] = "semi-annual"
+) -> float:
     """
     Calculates monthly interest rate for different compounding methods.
 
     >>> rate = 5/100
     >>> (rate / 12)*100
     0.41666...
+    >>> rate_calc(rate, "month")*100
+    0.41666...
     >>> rate_calc(rate, "semi-annual")*100
     0.41239...
     """
     r = rate_annual
-    match compound:
-        case "semi-annual":
-            # From https://www.contextures.com/excelpmtfunction.html, example 2
-            # In Canada, mortgage interest rates are compounded semi-annually, by law.
-            # https://www.nesto.ca/featured-articles/mortgage-principal-and-interest-explained/
-            return (r / 2 + 1) ** (1 / 6) - 1
+    if compound == "semi-annual":
+        periods = 2
+    elif compound == "month":
+        periods = 12
+    else:
+        assert_never(compound)
+    months_per_period = 12 / periods
+    # From https://www.contextures.com/excelpmtfunction.html, example 2
+    # In Canada, mortgage interest rates are compounded semi-annually, by law.
+    # https://www.nesto.ca/featured-articles/mortgage-principal-and-interest-explained/
+    return (r / periods + 1) ** (1 / months_per_period) - 1
+
+
+@dataclasses.dataclass
+class AmortizeItem:
+    payment_number: int
+    payment: float
+    principal: float
+    interest: float
+    principal_total: float
+    interest_total: float
+    balance: float
+
+    def rounded(self) -> AmortizeItem:
+        return AmortizeItem(
+            payment_number=self.payment_number,
+            payment=self.payment,
+            principal=round(self.principal),
+            interest=round(self.interest),
+            principal_total=round(self.principal_total),
+            interest_total=round(self.interest_total),
+            balance=round(self.balance),
+        )
+
+
+MONTHS_PER_YEAR = 12
+
+
+def amortize(
+    loan_amount: float,
+    rate_monthly: float,
+    payments_count: int,
+    *,
+    last: AmortizeItem | None = None,
+) -> list[AmortizeItem]:
+    number = 1 if last is None else last.payment_number + 1
+    principal_total = 0 if last is None else last.principal_total
+    interest_total = 0 if last is None else last.interest_total
+    payment = PMT(rate_monthly, payments_count, loan_amount)
+    ret: list[AmortizeItem] = []
+    balance = loan_amount
+    while True:
+        # Accumulate values instead of using IPMT, like in https://github.com/jbmohler/mortgage/blob/e4ecef1eeeceebfc51f3421f077727e5abefee17/mortgage.py#L62
+        interest = balance * rate_monthly
+        interest_total += interest
+        principal = payment - interest
+        principal_total += principal
+        balance -= principal
+        ret.append(
+            AmortizeItem(
+                payment_number=number,
+                payment=round(payment, 2),
+                principal=round(principal, 2),
+                interest=round(interest, 2),
+                principal_total=round(principal_total, 2),
+                interest_total=round(interest_total, 2),
+                balance=round(balance, 2),
+            )
+        )
+        if balance <= 0:
+            break
+        number += 1
+    return ret
--- mortgage_calc/test_financial.py
+++ mortgage_calc/test_financial.py
@@ -1,6 +1,6 @@
 import doctest
 from . import financial
-from .financial import PMT, rate_calc
+from .financial import IPMT, PMT, AmortizeItem, amortize, rate_calc
 
 
 def test_doctest() -> None:
@@ -9,11 +9,11 @@ def test_doctest() -> None:
 
 
 class Test_PMT:
-    def test_excel_example(self) -> None:
+    def test_example_excel(self) -> None:
         """
         Numbers from https://support.microsoft.com/en-us/office/pmt-function-0214da64-9a63-4996-bc20-214433fa6441.
         """
-        rate_annual = 0.08
+        rate_annual = 8 / 100
         payments_count = 10
         principal = 10_000
 
@@ -21,8 +21,25 @@ class Test_PMT:
         assert round(result, 2) == 1_037.03
 
 
+class Test_IPMT:
+    def test_example_excel(self) -> None:
+        """
+        Numbers from https://support.microsoft.com/en-au/office/ipmt-function-5cce0ad6-8402-4a41-8d29-61a0b054cb6f
+        """
+        rate_annual = 10 / 100
+        period = 1
+        years = 3
+        loan = 8_000
+
+        result = IPMT(rate_annual / 12, period, years * 12, loan)
+        assert round(result, 2) == 66.67
+
+        result = IPMT(rate_annual, 3, years, loan)
+        assert round(result, 2) == 292.45
+
+
 class Test_rate_calc:
-    def test_contextures_example(self) -> None:
+    def test_example_contextures(self) -> None:
         """
         Numbers from https://www.contextures.com/excelpmtfunction.html, example 2
         """
@@ -34,11 +51,51 @@ class Test_rate_calc:
 
         assert round(result, 2) == 657.13
 
-    def test_td_bank_example(self) -> None:
+
+class Test_amortize:
+    def test_example_centris(self) -> None:
         """
-        From https://apps.td.com/mortgage-payment-calculator/, with:
-        - mortgage = 300_000
-        - 3 year fixed rate @ 6.71%
-        - 25 years
-        - monthly payments
+        From https://www.centris.ca/en/tools/calculator
         """
+        mortgage = 300_000
+        rate_annual = 5.00 / 100
+        rate_monthly = rate_calc(rate_annual)
+        months = 25 * 12
+
+        schedule = amortize(mortgage, rate_monthly, months)
+
+        print(
+            "\n".join(
+                [
+                    f"{i.payment_number} {i.principal} {i.interest} {i.balance}"
+                    for i in (i.rounded() for i in schedule)
+                ]
+            )
+        )
+
+        first = schedule[0]
+        assert first.rounded() == AmortizeItem(
+            payment_number=1,
+            payment=1_744.81,
+            principal=508,
+            interest=1237,
+            principal_total=508,
+            interest_total=1237,
+            balance=299_492,
+        )
+
+        second = schedule[1]
+        assert second.rounded() == AmortizeItem(
+            payment_number=2,
+            payment=1_744.81,
+            principal=510,
+            interest=1235,
+            principal_total=round(first.principal + second.principal),
+            interest_total=round(first.interest + second.interest),
+            balance=298983,
+        )
+
+        assert len(schedule) == months
+
+        last = schedule[-1].rounded()
+        assert f"{last.principal_total} {last.balance} {last.interest_total}" == "300000 0 223444"
